0538133312948aba5545debf7f59bb0b972513ee
Prasanna venkatesh <115142768+Prasanna-aardae@users.noreply.github.com>
2023-02-08 19:04:53 +0530

    5 commit

--- src/main/java/fb_data_converter/FbDataConverter.java
+++ src/main/java/fb_data_converter/FbDataConverter.java
@@ -3,10 +3,9 @@ package fb_data_converter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -14,83 +13,87 @@ import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Image;
-import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.pdf.PdfWriter;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.*;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+
 public class FbDataConverter {
 	public static void main(String[] args) throws IOException, ParseException, DocumentException {
-		String file = "//Users//prasanna//Downloads//facebook-chitrabala39.zip";
+		String file = "/Users/prasanna/Downloads/facebook-100090052664579.zip";
 		File folder = fileToFolder(file);
 		File jsonFile = getJsonFile(folder);
 		FileSeparator pdfFilePath = new FileSeparator(file, '/', '.');
-		CreateNewTxtFile(pdfFilePath.path()+"/"+pdfFilePath.filename()+".pdf");
+		CreateNewPdfFile(pdfFilePath.path() + "/" + pdfFilePath.filename() + ".pdf");
 		@SuppressWarnings({ "rawtypes" })
 		List<HashMap> jsonReaderData = jsonReader(jsonFile);
-		
 		jsonToPdf(file, jsonReaderData);
 	}
 
-	  public static void jsonToPdf(String path,@SuppressWarnings("rawtypes") List<HashMap> jsonReaderData) throws DocumentException{
-	         
-		  try {
-				FileSeparator pdfFilePath = new FileSeparator(path, '/', '.');
-				Document document = new Document();
-				PdfWriter.getInstance(document, new FileOutputStream(pdfFilePath.path()+"/"+pdfFilePath.filename()+".pdf"));
-		        document.open();
-				jsonReaderData.forEach(jsonValues ->{
-			        Image img;
-					try {
-						img = Image.getInstance(pdfFilePath.path()+"/"+pdfFilePath.filename()+"/"+jsonValues.get("uri"));
+	public static void jsonToPdf(String path, @SuppressWarnings("rawtypes") List<HashMap> jsonReaderData) {
 
-						float scaler = ((document.getPageSize().getWidth() - document.leftMargin()
-					               - document.rightMargin() - 2) / img.getWidth()) * 50;
-				
-						img.scalePercent(scaler);
-						document.add(new Paragraph("Description : "+ jsonValues.get("description")));
-						document.add(new Paragraph("Title : "+ jsonValues.get("title")));
-						document.add(img);
-					} catch (DocumentException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (MalformedURLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+		String[] headers = new String[] { "Id", "Description", "post" };
 
-				});
-		        document.close();
-		        System.out.println("Done");
-		    } catch (IOException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-		  }
-	 
+		Document document = new Document(PageSize.LETTER.rotate());
+		FileSeparator pdfFilePath = new FileSeparator(path, '/', '.');
+		try {
+			PdfWriter.getInstance(document,
+					new FileOutputStream(pdfFilePath.path() + "/" + pdfFilePath.filename() + ".pdf"));
+			document.open();
+			Font fontHeader = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
+			Font fontRow = new Font(Font.FontFamily.TIMES_ROMAN, 10, Font.NORMAL);
+
+			PdfPTable table = new PdfPTable(headers.length);
+			for (String header : headers) {
+				PdfPCell cell = new PdfPCell();
+				cell.setGrayFill(0.9f);
+				cell.setPhrase(new Phrase(header.toUpperCase(), fontHeader));
+				table.addCell(cell);
+			}
+			table.completeRow();
+
+			jsonReaderData.forEach(jsonValues -> {
+				if(jsonValues.get("post") != null) {
+					Phrase phrase = new Phrase(String.valueOf(jsonReaderData.indexOf(jsonValues) + 1), fontRow);
+					table.addCell(new PdfPCell(phrase));
+					Phrase phrase1 = new Phrase(
+							jsonValues.get("description") == null ? "" : jsonValues.get("description").toString(), fontRow);
+					table.addCell(new PdfPCell(phrase1));
+					Phrase phrase2 = new Phrase(jsonValues.get("post").toString(),
+							fontRow);
+					table.addCell(new PdfPCell(phrase2));
+				}
+			});
+			table.completeRow();
+			document.addTitle("PDF Table Demo");
+			document.add(table);
+		} catch (DocumentException | FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			document.close();
+			System.out.println("Done");
+		}
+	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static List<HashMap> jsonReader(File jsonFile) throws IOException, ParseException {
 		JSONParser jsonParser = new JSONParser();
 		List<HashMap> mediaObjects = new ArrayList<HashMap>();
-
+		
 		try (FileReader reader = new FileReader(jsonFile)) {
 			Object jsonToObj = jsonParser.parse(reader);
 			JSONArray jsonobjToArray = (JSONArray) jsonToObj;
-			jsonobjToArray.forEach(jsonValues ->{
-				ParsePostObject((JSONObject) jsonValues).forEach(ds -> 
-				mediaObjects.add(ds));
+			jsonobjToArray.forEach(jsonValues -> {
+				ParsePostObject((JSONObject) jsonValues).forEach(ds -> mediaObjects.add(ds));
 			});
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -104,46 +107,15 @@ public class FbDataConverter {
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	private static List<HashMap> ParsePostObject(JSONObject posts) {
 		List<HashMap> mediaObjects = new ArrayList<HashMap>();
-		if (posts.get("attachments") != null) {
-	        ((ArrayList) posts.get("attachments")).forEach(emp ->{
-	        	PostsData((JSONObject) emp).forEach(ep-> mediaObjects.add((HashMap) ep));
-	        });
+		if (posts.get("data") != null) {
+			((ArrayList) posts.get("data")).forEach(emp -> {
+			mediaObjects.add((HashMap) emp);
+			});
 		}
-        return mediaObjects;
-	}
-
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	private static List<HashMap> PostsData(JSONObject data) {
-		List<HashMap> mediaObjects = new ArrayList<HashMap>();
-
-		((ArrayList) data.get("data")).forEach(emp -> {
-			try {
-				mediaObjects.add(DataMedia((JSONObject) emp));
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		});
 		return mediaObjects;
 	}
 
-	@SuppressWarnings("rawtypes")
-	private static HashMap DataMedia(JSONObject media) throws IOException {
-		JSONObject mediaObj = (JSONObject) media.get("media");
-		HashMap<String, String> mainValues = new HashMap<String, String>();
-
-		// Input the values
-		Object title = mediaObj.get("title");
-		mainValues.put("title", (String) title.toString());
-		Object uri = mediaObj.get("uri");
-		mainValues.put("uri", (String) uri.toString());
-		Object description = mediaObj.get("description");
-		mainValues.put("description", (String) description);
-		return mainValues;
-	}
-
-	public static void CreateNewTxtFile(String pdfPathName) throws IOException {
+	public static void CreateNewPdfFile(String pdfPathName) throws IOException {
 		try {
 			File myObj = new File(pdfPathName);
 			if (myObj.createNewFile()) {
@@ -155,40 +127,58 @@ public class FbDataConverter {
 			System.out.println("An error occurred.");
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public static File getJsonFile(File folder) throws IOException {
 		File[] listOfFiles = folder.listFiles();
-
 		for (File files : listOfFiles) {
 			FileSeparator jsonFile = new FileSeparator(files.getName(), '/', '.');
-			if (files.isFile() && jsonFile.extension().toString().compareTo("json") == 0) {
-				String[] tokens = jsonFile.filename().split("_");
-				if (Arrays.asList(tokens).contains("posts")) {
-					System.out.println(files);
-					return files;
+			if (jsonFile.extension().toString().compareTo("posts") == 0) {
+				for (File fi : files.listFiles()) {
+					FileSeparator jsonFileName = new FileSeparator(fi.getName(), '/', '.');
+					String[] tokens = jsonFileName.filename().split("_");
+					if (Arrays.asList(tokens).contains("posts")) {
+						return fi;
+					}
 				}
 			}
 		}
 		return folder;
-
 	}
 
 	public static File fileToFolder(String fileName) throws IOException {
 		FileInputStream file = new FileInputStream(fileName);
+		@SuppressWarnings("resource")
 		ZipInputStream zipFile = new ZipInputStream(file);
 		ZipEntry openZip;
+		FileSeparator myFile = new FileSeparator(fileName, '/', '.');
 		File folder = null;
+		folder = new File(myFile.path().toString() + "/" + myFile.filename().toString());
+
+		boolean bool = folder.mkdir();
+		byte[] buffer = new byte[1024];
+		if (bool) {
+			System.out.println("Folder is created successfully");
+		} else {
+			System.out.println("Error Found!");
+		}
 
 		while ((openZip = zipFile.getNextEntry()) != null) {
-			if (openZip.toString().compareTo("posts/") == 0) {
-				FileSeparator myFile = new FileSeparator(fileName, '/', '.');
-				folder = new File(myFile.path() + "//" + myFile.filename() + "//" + openZip.toString());
+			String name = openZip.getName();
+			File files = new File(
+					myFile.path() + File.separator + myFile.filename().toString() + File.separator + name);
+			if (new File(files.getParent()).mkdirs()) {
+				FileOutputStream FoS = new FileOutputStream(files);
+				int len;
+				while ((len = zipFile.read(buffer)) > 0) {
+					FoS.write(buffer, 0, len);
+				}
+				FoS.close();
+				zipFile.closeEntry();
+				openZip = zipFile.getNextEntry();
 			}
-			zipFile.closeEntry();
 		}
-		zipFile.close();
 		return folder;
 	}
 }
